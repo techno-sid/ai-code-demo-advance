556a1f1fc662be3f536ca6ce43e4ef8b0ee05a0d
techno-sid <siddhesh.divekar90@gmail.com>
2026-07-29 02:08:38 -0700

    Add files via upload

--- mslearn-ai-language-main/Labfiles/03-gen-ai-speech/Python/transcribe-speech/transcribe-speech.py
+++ mslearn-ai-language-main/Labfiles/03-gen-ai-speech/Python/transcribe-speech/transcribe-speech.py
@@ -4,7 +4,9 @@ from playsound3 import playsound
 from dotenv import load_dotenv
 
 # Import namespaces
-
+# import namespaces
+from openai import AzureOpenAI
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
 
 
 def main():
@@ -22,11 +24,28 @@ def main():
         playsound(file_path)
         
         # Create the Azure OpenAI client
+        # Create the Azure OpenAI client
+        token_provider = get_bearer_token_provider(                    
+            DefaultAzureCredential(), "https://ai.azure.com/.default"
+        )
 
+        client = AzureOpenAI(
+            azure_endpoint=endpoint,
+            azure_ad_token_provider = token_provider,
+            api_version="2025-03-01-preview"
+        )
 
         
         # Call model to transcribe audio file
+        # Call model to transcribe audio file
+        audio_file = open(file_path, "rb")
+        transcription = client.audio.transcriptions.create(
+            model=model_deployment,
+            file=audio_file,
+            response_format="text"
+        )
 
+        print(transcription)
 
 
 
